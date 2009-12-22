fb871985d5854769668b974eea34c15e9f96fb3e
rcirerol <rcirerol@b8a5a2a4-1f38-11de-9c9a-2d325a3e6494>
2009-12-22 06:28:52 +0000

    Fixing bug where injected dependency is not used. Can no longer have partial mocked ClassUnderTest by default.

--- trunk/CodeProgression.Framework.Testing/src/CodeProgression.Framework.Testing/SpecificationFor.cs
+++ trunk/CodeProgression.Framework.Testing/src/CodeProgression.Framework.Testing/SpecificationFor.cs
@@ -3,20 +3,16 @@ namespace CodeProgression.Framework.Testing
     public abstract class SpecificationFor<T> where T: class
     {
         protected static ClassUnderTest<T> AutoMocker;
-        protected static T ClassUnderTest;
+        protected static T ClassUnderTest { get { return AutoMocker.Instance; } }
 
         protected SpecificationFor()
         {
             AutoMocker = AutoMockFactory.CreateTarget<T>();
-            AutoMocker.PartialMockTheClassUnderTest();
-            ClassUnderTest = AutoMocker.Instance;
         }
 
         protected SpecificationFor(AutoMockType type)
         {
             AutoMocker = AutoMockFactory.CreateTarget<T>(type);
-            AutoMocker.PartialMockTheClassUnderTest();
-            ClassUnderTest = AutoMocker.Instance;
         }
     }
 }
